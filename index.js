6143bbebe0c5d5eebc855810919a28ba27edf929
keranenkirill <keranen.kirill@gmail.com>
2024-02-17 13:07:05 +0200

    personsdata erotettu omaksi tiedostoksi, joka importataan

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
 const express = require("express");
+const persons = require("./personsdata");
 const app = express();
 
-let persons = [
-   {
-      "id": "0422",
-      "name": "Essi Nokko",
-      "number": "7845360011"
-    },
-    {
-      "id": "5158",
-      "name": "Eetu",
-      "number": "888888888"
-    },
-    {
-      "id": "740a",
-      "name": "Aarni",
-      "number": "11232245"
-    }
-];
+
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
+
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
@@ -35,6 +21,8 @@ app.get("/info", (request, response) => {
       Current Time: ${currentTime}`
    );
 });
+
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
