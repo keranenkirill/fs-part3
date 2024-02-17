63b55ffa9938e77a238cb5195482655d2806df50
keranenkirill <keranen.kirill@gmail.com>
2024-02-17 13:54:40 +0200

    3.3: puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -2,27 +2,38 @@ const express = require("express");
 const persons = require("./personsdata");
 const app = express();
 
-
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
-
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
 app.get("/info", (request, response) => {
-   const currentTime = new Date();
-   response.send(
-      `Number of persons in the list: ${persons.length}
+  const currentTime = new Date();
+  response.send(
+    `Number of persons in the list: ${persons.length}
       <br>
       Current Time: ${currentTime}`
-   );
+  );
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  const person = persons.find((person) => person.id === id);
+  console.log(person);
 
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).send("No person in book by the given ID.");
+    console.log("No person in book by the given ID.");
+  }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
--- personsdata.js
+++ personsdata.js
@@ -1,19 +1,19 @@
 const persons = [
-   {
-      "id": "0422",
-      "name": "Essi Nokko",
-      "number": "7845360011"
-    },
-    {
-      "id": "5158",
-      "name": "Eetu",
-      "number": "888888888"
-    },
-    {
-      "id": "740a",
-      "name": "Aarni",
-      "number": "11232245"
-    }
+  {
+    id: 1,
+    name: "Essi Nokko",
+    number: "7845360011"
+  },
+  {
+    id: 2,
+    name: "Eetu",
+    number: "888888888"
+  },
+  {
+    id: 3,
+    name: "Aarni",
+    number: "11232245"
+  },
 ];
 
 module.exports = persons;
