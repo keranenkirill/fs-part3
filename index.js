1659f127a7d11ef9817f5924d5db8679ba8a0dfb
keranenkirill <keranen.kirill@gmail.com>
2024-02-17 12:57:43 +0200

    3.2: puhelinluettelon backend step2

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+   const currentTime = new Date();
+   response.send(
+      `Number of persons in the list: ${persons.length}
+      <br>
+      Current Time: ${currentTime}`
+   );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
