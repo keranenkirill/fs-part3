d3552e6172d75b04c4db042e8220280175b0f5be
keranenkirill <keranen.kirill@gmail.com>
2024-02-18 18:22:53 +0200

    3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require("express");
-const persons = require("./personsdata");
+let persons = require("./personsdata");
 const app = express();
 
 app.use(express.json());
+
+const generateID = () => {
+  do {
+    // generoidaan arvo
+    randomID = Math.floor(Math.random() * 1000000);
+  } while (
+    // tsekataan onko jo olemassa, toistetaan looppi jos on vastaava arvo olemassa
+    persons.some((person) => person.id === randomID)
+  );
+  return randomID;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    // jos nimi tai numero ovat ""
+    console.log("name or number missing");
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+  const person = {
+    id: generateID(),
+    name: body.name,
+    number: body.number,
+  };
+  console.log(persons);
+  console.log("adding: ", person);
+  persons = persons.concat(person);
+  console.log("updated phonebook:");
+  console.log(persons);
+  response.json(person);
+});
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -16,8 +49,8 @@ app.get("/info", (request, response) => {
   const currentTime = new Date();
   response.send(
     `Number of persons in the list: ${persons.length}
-      <br>
-      Current Time: ${currentTime}`
+     <br>
+     Current Time: ${currentTime}`
   );
 });
 
@@ -35,24 +68,24 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-   console.log(persons)
-   const id = Number(request.params.id)
-   let person = persons.find((person) => person.id === id);
-   
-   if (person){
-      console.log("deleting...")
-      console.log(person.id, person.name)
+app.delete("/api/persons/:id", (request, response) => {
+  console.log(persons);
+  const id = Number(request.params.id);
+  let person = persons.find((person) => person.id === id);
 
-      persons_after_delete = persons.filter(person => person.id !== id)
-      response.status(204).end()
-      
-      console.log('deleted succesfully')
-      console.log(persons_after_delete)
-   } else{
-      response.status(404).send("No person in book by the given ID.");
-   }
- })
+  if (person) {
+    console.log("deleting...");
+    console.log(person.id, person.name);
+
+    persons_after_delete = persons.filter((person) => person.id !== id);
+    response.status(204).end();
+
+    console.log("deleted succesfully");
+    console.log(persons_after_delete);
+  } else {
+    response.status(404).send("No person in book by the given ID.");
+  }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
