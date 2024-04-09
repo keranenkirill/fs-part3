0a50ca17c78707033366325c8b4d90afe0740dc7
Kirill Keränen <Keranen.kirill@gmail.com>
2024-04-09 13:15:29 +0300

    3.20*: puhelinluettelo ja tietokanta, step8

--- modules/phonebook.js
+++ modules/phonebook.js
@@ -7,7 +7,6 @@ const url = process.env.MONGODB_URI;
 console.log("connecting to", url);
 mongoose
   .connect(url)
-
   .then((result) => {
     console.log("connected to MongoDB");
   })
@@ -24,9 +23,15 @@ const phonebookSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
-  },
-
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^(\d{2,3}-\d+)$/.test(v); 
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Phone number required']
+  }
 });
 
 phonebookSchema.set("toJSON", {
@@ -34,7 +39,7 @@ phonebookSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-  },
+  }
 });
 
 module.exports = mongoose.model("PhoneBook", phonebookSchema);
